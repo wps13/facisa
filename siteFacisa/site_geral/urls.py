660b33e480f226c77bbd88563fa600e0e33390ae
wps13 <willanepaiva@hotmail.com>
2018-05-16 11:07:46 -0300

    remoção do cadastro

--- siteFacisa/site_geral/urls.py
+++ siteFacisa/site_geral/urls.py
@@ -4,7 +4,6 @@ from site_geral import views
 urlpatterns = [
     path('',views.inicio,name='inicio'),
     path('inicio/', views.inicio, name = 'inicio'),
-    path('cadastro/',views.cadastro, name = 'cadastro'),
     path('sobre/', views.sobre, name = 'sobre'), 
     path('acessos/', views.acessos, name='acessos'),
 ]
--- siteFacisa/site_geral/views.py
+++ siteFacisa/site_geral/views.py
@@ -3,10 +3,7 @@ from site_geral.models import AccessRecord, User
 
 def inicio(request):
     return render(request, 'site_geral/inicio.html')
-
-def cadastro(request):
-    return render(request, 'site_geral/cadastro.html')
-
+    
 def sobre(request):
     return render(request, 'site_geral/about.html')
 
