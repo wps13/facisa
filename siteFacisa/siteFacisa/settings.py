119e3d7caf2abd7465067caace73f314b56ab438
wps13 <willanepaiva@hotmail.com>
2018-05-18 13:40:25 -0300

    Herança de templates aplicada

--- siteFacisa/siteFacisa/settings.py
+++ siteFacisa/siteFacisa/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'site_geral',
 ]
 
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
+
 ]
 
 ROOT_URLCONF = 'siteFacisa.urls'
@@ -122,3 +125,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+INTERNAL_IPS = ['127.0.0.1']
--- siteFacisa/siteFacisa/urls.py
+++ siteFacisa/siteFacisa/urls.py
@@ -17,9 +17,16 @@ from django.contrib import admin
 from django.urls import path,include
 
 #from django.conf.urls import include
+from django.conf import settings
 from site_geral import views
 
 urlpatterns = [
     path('', include('site_geral.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls) )
+    ] + urlpatterns
